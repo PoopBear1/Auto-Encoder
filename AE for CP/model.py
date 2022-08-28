a94761a484bfa4d23a0ad13bbf219673dc62c168
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 <isaaczhang11@gmail.com>
2022-08-28 23:53:46 +1000

    get lowest loss, but need to visualize on CP

--- AE for CP/model.py
+++ AE for CP/model.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from torch.autograd import Variable
-import numpy as np
 from torchvision import transforms
 import os
 import torch.optim as optim
 import torch.nn.functional as F
 from torchvision.utils import save_image
 from torchsummary import summary
+
 
 class AutoEncoder(nn.Module):
     def __init__(self, num_points) -> None:
@@ -21,9 +21,9 @@ class AutoEncoder(nn.Module):
         # num_point = point_cloud.get_shape()[1].value
         # point_dim = point_cloud.get_shape()[2].value
 
-        # input: BxNx3 batch_size(32)*2500*3
+        # input: Bx3xN batch_size(32)*3*2500
 
-        # output: BxNx3
+        # output: Bx3xN
         self.encoder = nn.Sequential(
             nn.Conv1d(3, 64, kernel_size=1),
             nn.BatchNorm1d(64),
@@ -62,9 +62,9 @@ class AutoEncoder(nn.Module):
         )
 
     def forward(self, x):
+        # print("Original shape is : ", x.shape)
         batch_size, point_dim, n_points = x.shape[0], x.shape[1], x.shape[2]
         # print(batch_size, point_dim, n_points)
-        # print("Original shape is : ", x.shape)
 
         encoder = self.encoder(x)
         # print("After encoding  the shape is : ", encoder.shape)
@@ -81,32 +81,94 @@ class AutoEncoder(nn.Module):
         decoder = torch.reshape(decoder, (batch_size, point_dim, n_points))
         # print("back decoding, the shape is : ", decoder.shape)
 
-        return decoder, global_feat
-        # return out
+        # return decoder, global_feat
+        return decoder
 
 
 
-if torch.cuda.is_available():
-    from chamfer_distance.chamfer_distance_gpu import ChamferDistance # https://github.com/chrdiller/pyTorchChamferDistance
-    device = torch.device("cuda")
-else:
-    from chamfer_distance.chamfer_distance_cpu import ChamferDistance # https://github.com/chrdiller/pyTorchChamferDistance
-    device = torch.device("cpu")
 
-# summary(AutoEncoder(2500).to(device), input_size=(3, 2500))
 
-def train(train_loader,valid_loader,config):
-    model = AutoEncoder(config["num_points"])
-    loss_function = ChamferDistance()
-    for epoch in config["num_epochs"]:
+def train(train_loader, valid_loader, config):
+    device = config["device"]
+    loss_function = config["loss_function"]
+    model = AutoEncoder(config["num_points"]).to(device)
+    optimizer = optim.Adam(
+        model.parameters(),
+        lr=config["lr"],
+        weight_decay=config["regular_constant"],
+    )
+    train_loss_value = []
+    validate_loss_value = []
+    train_accuracy_value = []
+    validate_accuracy_value = []
+    current_epoch = []
+    low_loss = torch.tensor(float('inf')).cuda()
 
+    print("####### Training Processing on {}#######".format(device))
+    for epoch in range(config["num_epochs"]):
+        model.train()
+        train_loss = 0
+        train_accuracy = 0
+        current_epoch.append(epoch + 1)
+        total = 0
         for batch_idx, (inputs, targets) in enumerate(train_loader):
-            pass
+            optimizer.zero_grad()
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            # print(outputs[0].shape, "here\nhere", inputs[0].shape)
+            dist1, dist2 = loss_function(outputs, inputs)
+            loss = (torch.mean(dist1) + torch.mean(dist2))
+            loss.backward()
+            loss_value = loss.item()
+            optimizer.step()
+            train_loss += loss_value
+            total += targets.size(0)
 
-        pass
+        train_loss /= len(train_loader.dataset)
+        train_loss_value.append(loss_value)
 
+        # Validation step
+        model.eval()
+        validation_loss = 0
+        for batch_idx, (inputs, targets) in enumerate(valid_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            dist1, dist2 = loss_function(outputs, inputs)
+            validation_loss += (torch.mean(dist1) + torch.mean(dist2)).item()
+        validation_loss /= len(valid_loader.dataset)
+        print("\nIn epoch: ", epoch + 1)
+        print("\nTraining set: Avg. loss: {:.6f}".format(train_loss))
+        print("\nValidation set: Avg. loss: {:.6f}".format(validation_loss))
+        validate_loss_value.append(validation_loss)
+
+        if validation_loss < low_loss:
+            low_loss = validation_loss
+
+            torch.save(model.state_dict(), os.path.join(os.getcwd(), "ckpt.pth"))
+            print("model save at checkpoint")
+
+    plt.plot(current_epoch, train_loss_value, "b", label="Training Loss")
+    plt.savefig(os.path.join(os.getcwd(), "loss_curve.jpg"))
+    plt.figure()
     return model
 
-def test(test_loader):
 
-    return
+def test(test_loader, model, config):
+    model.eval()
+    test_loss = 0
+    device = config["device"]
+    loss_function = config["loss_function"]
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            dist1, dist2 = loss_function(output, data)
+            loss = (torch.mean(dist1) + torch.mean(dist2))
+            test_loss += loss.item()
+        test_loss /= len(test_loader.dataset)
+
+    print("\nTest set: Avg. loss: {:.6f}".format(test_loss))
+
+# device = "cuda"
+# summary(AutoEncoder(2500).to(device), input_size=(3, 2500), batch_size=32)
--- AE for CP/readfile.py
+++ AE for CP/readfile.py
@@ -1,11 +1,10 @@
-from cProfile import label
 import cv2
 import os
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
+from model import *
+# import show3d_balls
 
-
-# from torchvision import transforms
 
 def pc_normalize(pc):
     centroid = np.mean(pc, axis=0)
@@ -16,7 +15,7 @@ def pc_normalize(pc):
 
 
 class CP_file():
-    def __init__(self, root, npoints=2500, class_choice=None, normalize=True):
+    def __init__(self, root, npoints=2400, class_choice=None, normalize=True):
         self.normalize = normalize
         self.npoints = npoints
         self.data_path = os.path.join(root, "shapenetcore_partanno_segmentation_benchmark_v0")
@@ -60,9 +59,9 @@ class CP_file():
             if self.normalize:
                 point_set = pc_normalize(data)
 
-            choice = np.random.choice(len(label), self.npoints, replace=True)
             # resample
-            point_set = point_set[choice, :]
+            choice = np.random.choice(len(label), self.npoints, replace=True)
+            point_set = point_set[choice, :].reshape(-1, self.npoints)
             label = label[choice]
             data_set.append(point_set)
             label_set.append(label)
@@ -78,7 +77,7 @@ class CP_file():
         train_data, train_label = data_set[:train_len], label_set[:train_len]
 
         validation_data, validation_label = data_set[train_len + 1:train_len + validation_len], label_set[
-                                                                                                 train_len + 1:train_len + validation_len]
+                                                                                                train_len + 1:train_len + validation_len]
 
         test_data, test_label = data_set[
                                 train_len + validation_len + 1:train_len + validation_len + test_len], label_set[
@@ -94,6 +93,15 @@ class CP_file():
 
     def getChoice(self):
         return self.class_choice
+
+    def getTest(self):
+        length = len(self.meta[self.class_choice])
+        idx = np.random.choice(length)
+        testfile = np.loadtxt(self.meta[self.class_choice][idx][0])
+        choice = np.random.choice(len(testfile[0]), self.npoints, replace=True)
+        point_set = testfile[choice, :]
+
+        return point_set
 
     def __getitem__(self, index):
         point = self.meta[self.class_choice][index]
@@ -112,31 +120,44 @@ class CloudPointDataSet(Dataset):
         return len(self.labels)
 
     def __getitem__(self, index):
-        point = self.points[index][0]
-        label = self.labels[index][1]
+        points = self.points[index]
+        labels = self.labels[index]
 
         if self.transform:
-            point = self.transform(point)
+            points = self.transform(points)
 
         if self.target_transform:
-            label = self.target_transform(label)
+            labels = self.target_transform(labels)
 
-        return point, label
+        return points, labels
 
 
-def load_data(config):
-    base_dir = os.getcwd()
-    data_dir = os.path.join(base_dir, "data")
-    Choice = "Chair"
-    num_points = config["num_points"]
-    file = CP_file(data_dir, npoints=num_points, class_choice=Choice)
-    train_data, train_label, validation_data, validation_label, test_data, test_label = file.split_to_set()
+def load_data(file, config):
+    path = os.path.join(os.getcwd(), "exp_data")
+
+    if os.path.exists(path):
+        train_data = np.load(os.path.join(path, "train_data.npy"))
+        train_label = np.load(os.path.join(path, "train_label.npy"))
+
+        validation_data = np.load(os.path.join(path, "validation_data.npy"))
+        validation_label = np.load(os.path.join(path, "validation_label.npy"))
+
+        test_data = np.load(os.path.join(path, "test_data.npy"))
+        test_label = np.load(os.path.join(path, "test_label.npy"))
+    else:
+        os.mkdir("exp_data")
+        train_data, train_label, validation_data, validation_label, test_data, test_label = file.split_to_set()
+
+        np.save(os.path.join(path, "train_data.npy"), train_data)
+        np.save(os.path.join(path, "train_label.npy"), train_label)
+
+        np.save(os.path.join(path, "validation_data.npy"), validation_data)
+        np.save(os.path.join(path, "validation_label.npy"), validation_label)
+
+        np.save(os.path.join(path, "test_data.npy"), test_data)
+        np.save(os.path.join(path, "test_label.npy"), test_label)
 
     train_set = CloudPointDataSet(train_data, train_label)
-
-    # import show3d_balls
-    # show3d_balls.showpoints(train_data[0], ballradius=8)
-
     validation_set = CloudPointDataSet(validation_data, validation_label)
     test_set = CloudPointDataSet(test_data, test_label)
 
@@ -147,12 +168,66 @@ def load_data(config):
     )
 
 
-config = {
-    "lr": 1e-3,
-    "num_epochs": 1,
-    "num_points": 2500,
-    "batch_size": 32,
-    "regular_constant": 4e-8,
-}
+def trained_model_visualize(model, cloud_point, config):
+    cloud_point = torch.tensor(np.expand_dims(cloud_point, 0)).to(device=config["device"], dtype=torch.float)
+    # print(cloud_point.shape)
+    # print(cloud_point)
+    model.eval()
+    reconstructed_point = model(cloud_point).cpu().detach().numpy()
+    print("model output: ", reconstructed_point.shape)
+    reconstructed_point = np.squeeze(reconstructed_point).reshape(-1, 3)
+    print("the final output: ", reconstructed_point.shape)
+    np.savetxt("Recon_CP", reconstructed_point)
+    # show3d_balls.showpoints(reconstructed_point, ballradius=8)
 
-train_loader, valid_loader, test_loader = load_data(config)
+
+def main():
+    if torch.cuda.is_available():
+        from chamfer_distance.chamfer_distance_gpu import \
+            ChamferDistance  # https://github.com/chrdiller/pyTorchChamferDistance
+
+        device = torch.device("cuda")
+
+    else:
+        from chamfer_distance.chamfer_distance_cpu import \
+            ChamferDistance  # https://github.com/chrdiller/pyTorchChamferDistance
+
+        device = torch.device("cpu")
+
+    loss_function = ChamferDistance()
+
+    config = {
+        "lr": 1e-3,
+        "num_epochs": 90,
+        "num_points": 2500,
+        "batch_size": 32,
+        "regular_constant": 4e-8,
+        "device": device,
+        "loss_function": loss_function,
+    }
+
+    base_dir = os.getcwd()
+    data_dir = os.path.join(base_dir, "data")
+    Choice = "Chair"
+    num_points = config["num_points"]
+    file = CP_file(data_dir, npoints=num_points, class_choice=Choice)
+    cloud_point_testfile = file.getTest()
+    print("this is the original CP ", cloud_point_testfile.shape)
+    np.savetxt("Ori_sampledCP", cloud_point_testfile)
+    # show3d_balls.showpoints(cloud_point_testfile, ballradius=8)
+
+    if os.path.exists("ckpt.pth"):
+        checkpoint = torch.load("ckpt.pth", map_location=device)
+        model = AutoEncoder(num_points).to(device)
+        model.load_state_dict(checkpoint)
+    else:
+        train_loader, valid_loader, test_loader = load_data(file, config)
+        model = train(train_loader, valid_loader, config)
+        # show numerical loss result
+        test(test_loader, model, config)
+
+    # check model performance
+    trained_model_visualize(model, cloud_point_testfile.reshape(3, -1), config)
+
+
+main()
